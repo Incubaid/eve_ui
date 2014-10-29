fb0058b5c5d35ee2c39686b5f4c5a3f807381f66
Mohammad Tayseer <m.tayseer82@gmail.com>
2014-10-29 09:51:09 +0200

    Made sample data insertion faster using gevent

--- sample_data.py
+++ sample_data.py
@@ -1,27 +1,39 @@
-import requests
-requests.post('http://localhost:5000/people/', data={'name': 'Jo De Boek', 'email': 'jo@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Reem', 'email': 'reem@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Sherif', 'email': 'sherif@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Hendrik', 'email': 'hendrik@codescalers.com', 'born': 1982})
-requests.post('http://localhost:5000/people/', data={'name': 'KDS', 'email': 'kds@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Jan', 'email': 'jan@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Rob', 'email': 'rob@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Jonas', 'email': 'Jonas@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Sarah', 'email': 'Sarah@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Mohab', 'email': 'mohab@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Majik', 'email': 'majid@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Mahmoud Ali', 'email': 'mahmoudali@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Omnia', 'email': 'omnia@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Tamer', 'email': 'tamer@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Karim Sayed', 'email': 'ksayed@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Youssef', 'email': 'Youssef@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Hazem', 'email': 'Hazem@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Nabil', 'email': 'nabil@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Nayer', 'email': 'nayer@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Heba', 'email': 'heba@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Mogon', 'email': 'mogon@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Sameh', 'email': 'sameh@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Other mahmoud', 'email': 'other-mahmoud@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'Galal', 'email': 'galal@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'bishoy', 'email': 'bishoy@codescalers.com'})
-requests.post('http://localhost:5000/people/', data={'name': 'david', 'email': 'david@codescalers.com'})
+from gevent import monkey
+monkey.patch_all()
+import gevent
+
+import requests, time
+before = time.time()
+
+data = [
+    {'name': 'Jo De Boek', 'email': 'jo@codescalers.com'},
+    {'name': 'Reem', 'email': 'reem@codescalers.com'},
+    {'name': 'Sherif', 'email': 'sherif@codescalers.com'},
+    {'name': 'Hendrik', 'email': 'hendrik@codescalers.com', 'born': 1982},
+    {'name': 'KDS', 'email': 'kds@codescalers.com'},
+    {'name': 'Jan', 'email': 'jan@codescalers.com'},
+    {'name': 'Rob', 'email': 'rob@codescalers.com'},
+    {'name': 'Jonas', 'email': 'Jonas@codescalers.com'},
+    {'name': 'Sarah', 'email': 'Sarah@codescalers.com'},
+    {'name': 'Mohab', 'email': 'mohab@codescalers.com'},
+    {'name': 'Majik', 'email': 'majid@codescalers.com'},
+    {'name': 'Mahmoud Ali', 'email': 'mahmoudali@codescalers.com'},
+    {'name': 'Omnia', 'email': 'omnia@codescalers.com'},
+    {'name': 'Tamer', 'email': 'tamer@codescalers.com'},
+    {'name': 'Karim Sayed', 'email': 'ksayed@codescalers.com'},
+    {'name': 'Youssef', 'email': 'Youssef@codescalers.com'},
+    {'name': 'Hazem', 'email': 'Hazem@codescalers.com'},
+    {'name': 'Nabil', 'email': 'nabil@codescalers.com'},
+    {'name': 'Nayer', 'email': 'nayer@codescalers.com'},
+    {'name': 'Heba', 'email': 'heba@codescalers.com'},
+    {'name': 'Mogon', 'email': 'mogon@codescalers.com'},
+    {'name': 'Sameh', 'email': 'sameh@codescalers.com'},
+    {'name': 'Other mahmoud', 'email': 'other-mahmoud@codescalers.com'},
+    {'name': 'Galal', 'email': 'galal@codescalers.com'},
+    {'name': 'bishoy', 'email': 'bishoy@codescalers.com'},
+    {'name': 'david', 'email': 'david@codescalers.com'},
+]
+
+gevent.joinall([gevent.spawn(requests.post, 'http://localhost:5000/people', data=d) for d in data])
+
+print time.time() - before
