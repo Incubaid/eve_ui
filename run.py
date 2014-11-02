3a61b723d84f2ecd1dedf162f7127f2a35e6153e
Mohammad Tayseer <m.tayseer82@gmail.com>
2014-11-02 13:38:37 +0200

    Added docs for eve_docs replacement

--- run.py
+++ run.py
@@ -4,13 +4,17 @@ from flask.ext.bootstrap import Bootstrap
 from eve.render import send_response
 from eve_docs.config import get_cfg
 
+
 app = Eve(__name__)
 Bootstrap(app)
+
 
 @app.route('/ui')
 def ui():
     return render_template('ui.html')
 
+
+# Unfortunately, eve_docs doesn't support CORS (too bad!), so we had to reimplement it ourselves
 @app.route('/docs/spec.json')
 def specs():
     return send_response(None, [get_cfg()])
